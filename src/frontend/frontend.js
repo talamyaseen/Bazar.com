31bd31d4c328c6ea09ecec3794e43764ce6dc91c
talamyaseen <112306113+talamyaseen@users.noreply.github.com>
2024-03-22 23:30:19 +0200

    Update frontend.js

--- src/frontend/frontend.js
+++ src/frontend/frontend.js
@@ -10,7 +10,9 @@ app.get('/search/:topic',(req,res)=>{       // if the req is search req route it
     http.get('http://catalog:4000/search/'+req.params.topic,( response)=>{         // get req to the catalog server to return the item
         response.on("data", (chunk)=>{
             const responseData = JSON.parse(chunk);                                  // parse to json format
-            res.json(responseData)                                                   // return the response from catalog server 
+            res.json(responseData)                                                   // return the response from catalog server
+            console.log('Fetched successfully');
+            console.log(responseData);
         });     
 })
     }catch (error) {                                                                
@@ -23,6 +25,8 @@ app.get('/info/:item_number',(req,res)=>{                                       
         response.on("data", (chunk)=>{
             const responseData = JSON.parse(chunk);
             res.json(responseData)
+            console.log('Fetched successfully');
+            console.log(responseData);
         });   
 })
 }catch (error) {
@@ -34,6 +38,8 @@ app.get('/info/:item_number',(req,res)=>{                                       
 app.post('/purchase/:item_number', async (req,res)=>{                                                       // if the req is purchase req route it based on item number     
     try {
         const response = await axios.post(`http://order:5000/purchase/${req.params.item_number}`);      // make an http post req to order server using axios
+        console.log('Orderd successfully');
+        console.log(response.data);
         res.json(response.data);
     } catch (error) {
         res.status(500).json({ error: error.message });
